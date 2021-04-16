a5da6c5c6d4510703cf219ebb5b3398344926af5
akinolajaye <akinolajaye@gmail.com>
2021-04-16 18:26:18 +0100

    unique addresses created

--- home.ino
+++ home.ino
@@ -4,42 +4,38 @@
 #include <utility/Adafruit_MCP23017.h>
 #include <EEPROM.h>
 #include <avr/eeprom.h>
-#define DEB
+#define DEBU
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
-int HOUSE_LEN=10;
-char f[4]="";
-char fl[5]="";
-char g[6]="";
-char o[3]="";
-char t[5]="";
-char n[7]="";
-char q[6]="";
-char a[4]="";
-char v[3]="";
+int HOUSE_LEN=8;
+char floorRoom[4]="";
+char firstFlrRoom[4]="";
+char groundRoom[4]="";
+char outsideRoom[3]="";
+char typeSetting[4]="";
+char gardenSetting[5]="";
+char deviceName[2]="";
 int written=EEPROM.read(0);// reads the first value in the eeprom 
-char* house[10]={f,fl,g,o,t,n,q,a,v};
-typedef enum state_e {floors=0,first,ground,outside,type,names,qualifiers,actions,values} state_t;
+char* house[8]={floorRoom,firstFlrRoom,groundRoom,outsideRoom,typeSetting,gardenSetting,deviceName};
+typedef enum state_e {floors=0,first,ground,outside,type,gardenType,names} state_t;
 
-
+//int sumIterations(char*,char*);
 void writeEE(){//function to write house description into eeprom
-  char f[5]={'F','G','O','-'};
-  char fl[6]={'1','2','B','P','-'};
-  char g[7]={'K','H','L','3','4','-'};
-  char o[4]={'G','R','-'};
-  char t[6]={'L','A','H','W','-'};
-  char n[8]={'M','C','D','B','P','W','-'};
-  char q[7]={'1','2','3','L','R','-'};
-  char a[5]={'1','0','L','-'};
-  char  v[4]={'T','R','x'};
-  char* house[HOUSE_LEN]={f,fl,g,o,t,n,q,a,v};//stores pointers to the different char arrays
+  char floorRoom[5]={'F','G','O','-'};
+  char firstFlrRoom[5]={'1','2','B','-'};
+  char groundRoom[5]={'K','H','L','-'};
+  char outsideRoom[4]={'G','R','-'};
+  char typeSetting[5]={'L','A','H','-'};
+  char gardenSetting[6]={'L','A','H','W','-'};
+  char deviceName[3]={'M','x'};
+  char* house[HOUSE_LEN]={floorRoom,firstFlrRoom,groundRoom,outsideRoom,typeSetting,gardenSetting,deviceName};//stores pointers to the different char arrays
   int totalLen=0;
 
-  EEPROM.write(totalLen,1); //writes a 1 at the fl memory location to signify data has been written to
+  EEPROM.update(totalLen,1); //writes a 1 at the firstFlrRoom memory location to signify data has been written to
   for(int i=0;i<HOUSE_LEN-1;i++){
     for(int j=0;j<strlen(house[i]);j++){
       totalLen++;
-      EEPROM.write(totalLen,house[i][j]);
+      EEPROM.update(totalLen,house[i][j]);
       #ifdef DEBUG
       Serial.print(totalLen);
       Serial.print("     ");
@@ -55,9 +51,9 @@ void readEE(char** house){//reads the house desc from the eeprom
     for(int i=0;i<HOUSE_LEN-1;i++){//loop based on the amount of different house descriptors
       int j=0;
 
-      while((char) EEPROM.read(count)!='-'){//keep looping until it sees a '-' which signifies that it should move to the next descriptor
+      while((char) EEPROM.read(count)!='-'){//keep looping until it sees valueSetting '-' which signifies that it should move to the next descriptor
         
-        if((char) EEPROM.read(count)=='x'){break;}//breaks when it sees a 'x' which signifies the end of all the data being reae
+        if((char) EEPROM.read(count)=='x'){break;}//breaks when it sees valueSetting 'x' which signifies the end of all the data being reae
    
         house[i][j]=(char) EEPROM.read(count);
         j++;
@@ -92,7 +88,7 @@ void navSettings(char* setting,int option,int num_setting){
           break;
 
 
-        case 1://fl floor
+        case 1://firstFlrRoom floor
       
           if (setting[option]=='1'){
             lcd.print("1st Bedroom");
@@ -103,8 +99,6 @@ void navSettings(char* setting,int option,int num_setting){
           }else if(setting[option]=='B'){
               lcd.print("Bathroom");        
             
-          }else if(setting[option]=='P'){
-              lcd.print("Play Room");        
             
           }
           break;
@@ -118,18 +112,14 @@ void navSettings(char* setting,int option,int num_setting){
             lcd.print("Hall");
             
           }else if(setting[option]=='L'){
-              lcd.print("Lounge");        
+              lcd.print("Living Room");        
             
-          }else if(setting[option]=='3'){
-              lcd.print("3rd Bedroom");        
+          }     
             
-          }else if(setting[option]=='4'){
-              lcd.print("4th Bedroom");        
-            
-          }
+          
           break;
 
-        case 3://o
+        case 3://outsideRoom
       
           if (setting[option]=='G'){
             lcd.print("Garage");
@@ -151,87 +141,41 @@ void navSettings(char* setting,int option,int num_setting){
           }else if(setting[option]=='H'){
               lcd.print("Heat");        
             
+          }
+          break;
+
+        case 5://gardenType
+      
+          if (setting[option]=='L'){
+            lcd.print("Light");
+            
+          }else if (setting[option]=='A'){
+            lcd.print("Lamp");
+            
+          }else if(setting[option]=='H'){
+              lcd.print("Heat");        
+            
           }else if(setting[option]=='W'){
               lcd.print("Water");        
             
           }
           break;
-
-        case 5://names
+          
+        case 6://names
       
           if (setting[option]=='M'){
             lcd.print("Main");
             
-          }else if (setting[option]=='C'){
-            lcd.print("Ceiling");
-            
-          }else if(setting[option]=='D'){
-              lcd.print("Desk");        
-            
-          }else if(setting[option]=='B'){
-              lcd.print("Bed");        
-            
-          }else if(setting[option]=='P'){
-              lcd.print("Cupboard");        
-            
-          }else if(setting[option]=='W'){
-              lcd.print("Wall");        
-            
-          }
-          break;
-          
-        case 6://qualifiers
-      
-          if (setting[option]=='1'){
-            lcd.print("One");
-            
-          }else if (setting[option]=='2'){
-            lcd.print("Two");
-            
-          }else if(setting[option]=='3'){
-              lcd.print("Three");        
-            
-          }else if(setting[option]=='L'){
-              lcd.print("Left");        
-            
-          }else if(setting[option]=='R'){
-              lcd.print("Right");        
-            
           }
           break;
 
-        case 7://Action
-      
-          if (setting[option]=='1'){
-            lcd.print("On");
-            
-          }else if (setting[option]=='0'){
-            lcd.print("Off");
-            
-          }else if(setting[option]=='L'){
-              lcd.print("Level");        
-            
-          }
-          break;
-
-        case 8://Values
-      
-          if (setting[option]=='T'){
-            lcd.print("Time: ");
-            
-          }else if (setting[option]=='R'){
-            lcd.print("Range: ");
-            
-          }
-          break;
-   
       }
   
   }
 
 
 int chooseSettings(char* setting,int num_setting){//num setting is the number based on the enum which is used to specify what case will be called
-    int select=0;//select is a number that is used to call the index of a house descriptor in order to print out its value and return a number that can be used for cases
+    int select=0;//select is valueSetting number that is used to call the index of valueSetting house descriptor in order to print out its value and return valueSetting number that can be used for cases
    
     navSettings(setting,select,num_setting);
     bool chosen =false;
@@ -261,7 +205,7 @@ int chooseSettings(char* setting,int num_setting){//num setting is the number ba
           
         }else if(old_buttons&BUTTON_DOWN){
           
-          select=50;//chosen a random number to represent the case for going back
+          select=50;//chosen valueSetting random number to represent the case for going back
           chosen=true;
           
         }
@@ -274,14 +218,38 @@ int chooseSettings(char* setting,int num_setting){//num setting is the number ba
   return select;
 }
 
+int sumIterations(char* firstIter,char* nestedIter){
 
+  int sumTotal=0;
+
+  for (int i=1;i<=strlen(firstIter);i++){
+
+    for(int j=1;j<=strlen(nestedIter);j++){
+
+      sumTotal+=i+j+1;
+      
+    }
+    
+  }
+
+  return sumTotal;
+  
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16,2);
- 
-
+  /*
+  char x[10] ="";
+  String z="feejjt";
+  String outsideRoom ="hii";
+  z.toCharArray(x,10);
+  outsideRoom.toCharArray(x,10);
+  
+  
+  Serial.println(x);
+  */
 
   if (written ==0){
     writeEE();
@@ -306,20 +274,29 @@ void setup() {
   
 }
 
+
+
 void loop() {
   // put your main code here, to run repeatedly:
 
 
-  static state_t state=floors;
-  static state_t back;
+    
+  static int add,sumTotal,choice,modChoice,modValue,modProduct,sum,startingAddress;
 
-  static int choice;
+  static state_t state=floors;
+  static state_t back,secondBack;
+
 
   switch (state){
 
       case floors:
+        add=0;
+          
   
-        choice=chooseSettings(f,floors); //could have used house[floors] instead of f
+        choice=chooseSettings(floorRoom,floors); //could have used house[floors] instead of floorRoom
+
+        add=(choice+1)*200;
+        startingAddress=add;
         
         
   
@@ -328,16 +305,19 @@ void loop() {
           case 0:
   
             state= first;
+            
             break;
   
           case 1:
   
             state= ground;
+            
             break;
   
           case 2:
   
             state= outside;
+        
             break;
   
         }
@@ -346,8 +326,12 @@ void loop() {
         
   
       case first:
+        add=startingAddress;
   
-        choice=chooseSettings(fl,first);
+        choice=chooseSettings(firstFlrRoom,first);
+
+        add+=choice*50;
+
         
         if (choice==50){
           state=floors;// goes back to thee previous state
@@ -360,7 +344,11 @@ void loop() {
         
   
       case ground:
-        choice=chooseSettings(g,ground);
+        add=startingAddress;
+        
+        choice=chooseSettings(groundRoom,ground);
+
+        add+=choice*50;
         
         if (choice==50){
           state=floors;// goes back to thee previous state
@@ -373,24 +361,56 @@ void loop() {
         
   
       case outside:
-        choice=chooseSettings(o,outside);
+
+        add=startingAddress;
+        choice=chooseSettings(outsideRoom,outside);
+        add+=choice*50;
         
         if (choice==50){
           state=floors;// goes back to thee previous state
         }else{
-          state=type;
+          
+          if (outsideRoom[choice]=='R'){
+              state=gardenType;
+          }else{
+
+            state=type;
+          }
+          
           back=outside;
         }
         break;
 
 
+
+
       case type:
 
-        choice=chooseSettings(t,type);
+        
+
+        choice=chooseSettings(typeSetting,type);
+        modChoice=choice;//saves choice for mod
+        sumTotal=sumIterations(typeSetting,deviceName);
+
         if (choice==50){
           state=back;
         }else{
           state=names;
+          secondBack=type;
+        }
+        break;
+
+      case gardenType:
+
+        choice=chooseSettings(gardenSetting,gardenType);
+        modChoice=choice;//saves choice for mod
+        sumTotal=sumIterations(gardenSetting,deviceName);
+        
+        if (choice==50){
+          state=back;
+        }else{
+          state=names;
+          secondBack=gardenType;
         }
         break;
 
@@ -398,55 +418,29 @@ void loop() {
 
       case names:
 
-        choice = chooseSettings(n,names);
+        choice = chooseSettings(deviceName,names);
+
+
+
         
         if (choice==50){
-          state=type;
+          state=secondBack;
+          add-=sum;
         }else{
-          state=qualifiers;
+
+        
+
+          modValue=sumTotal/strlen(typeSetting);
+          modProduct=(modChoice*modValue);
+
+          sum=(modProduct+(modChoice+1)+(choice+1)+1) % (modValue*(modChoice+1)+1);
+
+          add+=sum;
+          Serial.println(add);
+          delay(100);
         }
         break;
-
-
-      case qualifiers:
-        choice=chooseSettings(q,qualifiers);
-
-        if (choice==50){
-
-          state=names;
-        }
-
-        break;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-
-        
+      
    
   }
 
